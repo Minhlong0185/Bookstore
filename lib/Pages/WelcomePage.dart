c73013d167d6e156f5c768c22fb4fd3bafe8c62a
Quang-Thanh <quangthanhvu711@gmail.com>
2024-04-04 22:56:31 +0700

    Commit WelcomePages 50%

--- lib/Pages/WelcomePage.dart
+++ lib/Pages/WelcomePage.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -6,10 +8,39 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(child: Padding(
-        padding: const EdgeInsets.all(10),
-        child: Column(children: [Text("Welcome Page")],),
-      )),
+      body: Column(
+        children: [
+          Container(
+            height: 500,
+            padding: EdgeInsets.all(20),
+            color: Theme.of(context).colorScheme.primary,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("Assets/Images/book.png",
+                    width: 380,),
+                    SizedBox(height: 60),
+                    Text("Book - Store", style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                      color: Theme.of(context).colorScheme.background,
+                    ),),
+                    SizedBox(height: 10),
+                    Flexible(
+                      child: Text("Here you can find best book for you and you can also read book and listen book", 
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                        color: Theme.of(context).colorScheme.background,
+                      ),),
+                    ),
+                  ],
+                ),
+              )
+            ],),
+          )
+        ],)
     );
   }
 }
