9484690b10c7acc432c22a387f74ef1907510134
Quang-Thanh <quangthanhvu711@gmail.com>
2024-04-05 22:10:40 +0700

    Commit WelcomePage full

--- lib/Pages/WelcomePage.dart
+++ lib/Pages/WelcomePage.dart
@@ -11,7 +11,7 @@ class WelcomePage extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            height: 500,
+            height: 450,
             padding: EdgeInsets.all(20),
             color: Theme.of(context).colorScheme.primary,
             child: Row(
@@ -21,6 +21,7 @@ class WelcomePage extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
+                    SizedBox(height: 50),
                     Image.asset("Assets/Images/book.png",
                     width: 380,),
                     SizedBox(height: 60),
@@ -39,6 +40,54 @@ class WelcomePage extends StatelessWidget {
                 ),
               )
             ],),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(children: [
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Disclaimer", style: Theme.of(context).textTheme.labelMedium,),
+              ],
+            ),
+            SizedBox(height: 10),
+            Row(
+                children: [
+                  Flexible(child: Text(
+                    "n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum", 
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.labelSmall,
+                  ),
+                  )
+              ],
+            ),
+            
+            ],),
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                  height: 55,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("CONTINOUE",
+                    style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: Theme.of(context).colorScheme.background,
+                    letterSpacing: 1.5,
+                    ),
+                    ),
+                ],),
+              ),
+            ),
           )
         ],)
     );
